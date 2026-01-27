137b254305e096f8b31aa36d304640023e715495
Pralin Khaira <be.coder.19@gmail.com>
2026-01-27 18:11:30 +0530

    Time: 216 ms (84.37%) | Memory: 289.9 MB (87.98%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
@@ -3,46 +3,46 @@ public:
     int minCost(int n, vector<vector<int>>& edges) {
         const int INF = INT_MAX / 2;
 
-        // Adjacency list: graph[u] = {v, cost}
         vector<vector<pair<int,int>>> graph(n);
-
-        // Add asymmetric edges
-        for (auto &e : edges) {
+        for (const auto &e : edges) {
             int u = e[0], v = e[1], w = e[2];
-            graph[u].push_back({v, w});
-            graph[v].push_back({u, w * 2});
+            graph[u].emplace_back(v, w);
+            graph[v].emplace_back(u, w * 2);
         }
 
-        // Distance array
         vector<int> dist(n, INF);
         dist[0] = 0;
 
-        // Min-heap for Dijkstra: {distance, node}
         priority_queue<
             pair<int,int>,
             vector<pair<int,int>>,
             greater<pair<int,int>>
         > pq;
 
-        pq.push({0, 0});
+        pq.emplace(0, 0);
 
         while (!pq.empty()) {
             auto [d, node] = pq.top();
             pq.pop();
 
-            if (d > dist[node]) continue;
+            // Skip outdated state
+            if (d != dist[node]) continue;
 
-            for (auto &edge : graph[node]) {
+            // Early exit: shortest path to destination found
+            if (node == n - 1) return d;
+
+            for (const auto &edge : graph[node]) {
                 int next = edge.first;
                 int cost = edge.second;
 
-                if (dist[next] > d + cost) {
-                    dist[next] = d + cost;
-                    pq.push({dist[next], next});
+                int newDist = d + cost;
+                if (newDist < dist[next]) {
+                    dist[next] = newDist;
+                    pq.emplace(newDist, next);
                 }
             }
         }
 
-        return dist[n - 1] >= INF ? -1 : dist[n - 1];
+        return -1;
     }
 };
